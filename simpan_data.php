f9c7641d3d7eca726115f026ea60a6afaf16f33f
Aditya-wardana12 <2300018155@webmail.uad.ac.id>
2024-07-05 19:17:22 +0700

    aditya155

--- simpan_data.php
+++ simpan_data.php
@@ -7,10 +7,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Data yang akan disimpan dalam format yang sesuai
     $data = "Nama Pemesan: $nama\nNama Buku: $nama_buku\nDisplay ke: $display_ke\n\n";
 
- 
+    // Path ke file untuk menyimpan data
     $file_path = "data_pemesanan.txt";
 
- 
+    // Menyimpan data ke file
+    if (file_put_contents($file_path, $data, FILE_APPEND | LOCK_EX) !== false) {
+        echo "Data berhasil disimpan.";
+    } else {
+        echo "Gagal menyimpan data.";
+    }
 }
-
 ?>
